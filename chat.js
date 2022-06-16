9e036e4be82c53625db70e23f325550ac26d2b29
FaisalTmz007 <faisaltemas007@gmail.com>
2022-06-16 18:44:31 +0700

    Unresponsive

---
+++ chat.js
@@ -0,0 +1,26 @@
+const popup = document.querySelector('.chat-popup');
+const chatBtn = document.querySelector('.chat-btn');
+const submitBtn = document.querySelector('.submit');
+const chatArea = document.querySelector('.chat-area');
+const inputElm = document.querySelector('input');
+
+
+//   chat button toggler 
+
+chatBtn.addEventListener('click', ()=>{
+    popup.classList.toggle('show');
+})
+
+// send msg 
+submitBtn.addEventListener('click', ()=>{
+    let userInput = inputElm.value;
+
+    let temp = `<div class="out-msg">
+    <span class="my-msg">${userInput}</span>
+    <img src="Icon/avatar-review.png" class="avatar">
+    </div>`;
+
+    chatArea.insertAdjacentHTML("beforeend", temp);
+    inputElm.value = '';
+
+})
---
+++ signin.js
@@ -0,0 +1,27 @@
+var objUser = [
+    {
+        email: "user@gmail.com",
+        pass: "user123"
+    },
+    {
+        email: "company@gmail.com",
+        pass: "company123"
+    }
+]
+
+function getInfo() {
+    var email = document.getElementById("email").value
+    var pass = document.getElementById("pass").value
+
+    
+        if(email == "user@gmail.com" && pass == "user123") {
+            window.location.href="home.html"
+            return
+        }
+        else if(email == "company@gmail.com" && pass == "company123") {
+            window.location.href="homepage-company.html"
+            return
+        }
+    
+    alert("incorrect username or password")
+}
